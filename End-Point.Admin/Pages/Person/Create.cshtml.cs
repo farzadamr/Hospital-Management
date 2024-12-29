9a60ea0b594550b2156a55c39ad12f811f1d3892
Farzad <amirifarzad1381@gmail.com>
2024-12-29 13:12:43 +0330

    config-services

---
+++ End-Point.Admin/Pages/Person/Create.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace End_Point.Admin.Pages.Person
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
