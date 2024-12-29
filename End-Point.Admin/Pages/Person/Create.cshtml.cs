c800395fd1047737bb74b4ebe4521d8ad64ccb07
Farzad <amirifarzad1381@gmail.com>
2024-12-29 13:44:23 +0330

    person-create

--- End-Point.Admin/Pages/Person/Create.cshtml.cs
+++ End-Point.Admin/Pages/Person/Create.cshtml.cs
@@ -1,13 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Dtos;
+using Services.Interfaces;
 
 namespace End_Point.Admin.Pages.Person
 {
     public class CreateModel : PageModel
     {
+        private readonly IPersonService personService;
+        public CreateModel(IPersonService personService)
+        {
+            this.personService = personService;
+        }
+        public ResultDto result { get; set; }
+        [BindProperty]
+        public PersonDto personModel { get; set; }
         public void OnGet()
         {
-
+            result = new ResultDto(false, "");
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                result = new ResultDto(false, "invalid model state");
+                return Page();
+            }
+            var insertedResult = await personService.AddPersonAsync(personModel);
+            result = insertedResult;
+            return Page();
         }
     }
 }
---
+++ End-Point.Admin/ServiceInjector/SetUp.cs
@@ -0,0 +1,18 @@
+﻿using Services.Interfaces;
+using Services.Services;
+
+namespace End_Point.Admin.ServiceInjector
+{
+    public static class SetUp
+    {
+        public static IServiceCollection InjectServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddTransient<IPersonService>(provider =>
+            {
+                return new PersonService(connectionString);
+            });
+            return services;
+        }
+    }
+}
