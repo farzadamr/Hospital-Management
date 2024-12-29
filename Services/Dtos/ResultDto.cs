11d828b76e943d0bc5e6bf806ba29d6c7f9d5328
Farzad <amirifarzad1381@gmail.com>
2024-12-29 13:17:38 +0330

    add-result-class

---
+++ Services/Dtos/ResultDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Dtos
+{
+    public class ResultDto
+    {
+        public ResultDto(bool isSuccess,string? Message)
+        {
+            this.iSuccess = isSuccess;
+            this.Message = Message;
+        }
+        public bool iSuccess { get; private set; }
+        public string? Message { get; private set; }
+    }
+    public class ResultDto<TData>
+    {
+        public ResultDto(TData Data,bool isSuccess,string? Message)
+        {
+            this.Data = Data;
+            this.iSuccess = isSuccess;
+            this.Message = Message;
+        }
+        public TData Data { get; private set; }
+        public bool iSuccess { get; private set; }
+        public string? Message { get; private set; }
+    }
+}
